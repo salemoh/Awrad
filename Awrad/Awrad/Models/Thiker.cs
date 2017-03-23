e2f6ca1ce4e9b9dfd1e54f448290d3bb7162f8d9
Mohammad Saleh <salemoh@gmail.com>
2017-03-23 09:05:46 -0700

    Added a thiker DB object Added a wird DB object

--- Awrad/Awrad/Models/Thiker.cs
+++ Awrad/Awrad/Models/Thiker.cs
@@ -8,15 +8,16 @@ namespace Awrad.Models
         public int Id { get; set; }
                 
         public int WirdType { get; set; } // Type of wird this thiker belongs to        
-        public int Type { get; set; } // Is this an Introduction, Quran, Sunnah, Summary thiker 
+        public int Type { get; set; } // Is this a Quran or Sunnah thiker 
         public int Order { get; set; } // The order in which the thiker appears in a wird
         public int Time { get; set; } // When do we recite this thiker Any, Morning, Evening
         public int Iterations { get; set; } // Average interations
         public int MinIerations { get; set; } // Minimum number of iterations
         public int MaxIterations { get; set; } // Maximum iterations
+        public int Size { get; set; } // Shirt size of the thiker. Small, Med, Large
         
         public string Content { get; set; } // Content of the thiker
-        public string Reference { get; set; } // The reference to a thiker
+        public string Referrence { get; set; } // The reference to a thiker
         public string Virtue { get; set; } // Virtue gained by reciting this thiker
     }
 }
---
+++ Awrad/Awrad/Models/Wird.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace Awrad.Models
+{
+    public class Wird
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Description { get; set; } // Description of the wird
+        public string Introduction { get; set; } // Wird intro
+        public string Summary { get; set; } // Summary of the wird
+        public string Accent { get; set; } // Location of image accents to display as border for wird
+    }
+}
+
