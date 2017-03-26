397b961e6f7a374f4efa2c45edd0466122b8bf4c
Mohammad Saleh <salemoh@gmail.com>
2017-03-26 01:38:23 -0700

    Now we only copy the DB in Android if doesn't exist or too small

--- Awrad/Awrad.Android/FileHelper.cs
+++ Awrad/Awrad.Android/FileHelper.cs
@@ -15,25 +15,35 @@ namespace Awrad.Droid
             string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
             path = Path.Combine(path, filename);
 
-            // If no DB then copy it from assets
-            // Get an instance of asset manager
-            var assets = Android.App.Application.Context.Assets;
-
-            using (var br = new BinaryReader(assets.Open(filename)))
-            {
-                using (var bw = new BinaryWriter(new FileStream(path, FileMode.Create)))
-                {
-                    byte[] buffer = new byte[2048];
-                    int length = 0;
-                    while ((length = br.Read(buffer, 0, buffer.Length)) > 0)
-                    {
-                        bw.Write(buffer, 0, length);
-                    }
-                }
-            }
+            // If no DB or DB too small then copy it from assets
+            CopyDbIfNotExist();
 
             // Return the path to the DB
             return path;
+
+            void CopyDbIfNotExist()
+            {
+                var fileSize = new FileInfo(path).Length;
+                if (!File.Exists(path) || fileSize < 100)
+                {
+                    // Get an instance of asset manager
+                    var assets = Android.App.Application.Context.Assets;
+
+                    using (var br = new BinaryReader(assets.Open(filename)))
+                    {
+                        using (var bw = new BinaryWriter(new FileStream(path, FileMode.Create)))
+                        {
+                            byte[] buffer = new byte[2048];
+                            int length = 0;
+                            while ((length = br.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                bw.Write(buffer, 0, length);
+                            }
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
