ec5d771c4a5ac132d5c5f20ec8c6f14ddc929ae7
salemoh <salemoh@gmail.com>
2017-04-19 16:08:53 -0700

    Image sharing is working for iOS

--- Awrad/Awrad.iOS/ShareHelperiOS.cs
+++ Awrad/Awrad.iOS/ShareHelperiOS.cs
@@ -8,15 +8,33 @@ using UIKit;
 using Awrad.Helpers;
 using Awrad.iOS;
 using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
 
 [assembly: Dependency(typeof(ShareHelperiOS))]
 namespace Awrad.iOS
 {
     class ShareHelperiOS : IShareHelper
     {
-        public void Share(string imageUrl)
+        public async void Share(string imageUrl)
         {
-            throw new NotImplementedException();
+            // Get the URI of the image
+            var imageUri = new Uri(imageUrl);
+            var imageSource = ImageSource.FromUri(imageUri);
+            var handler = new ImageLoaderSourceHandler();
+            var uiImage = await handler.LoadImageAsync(imageSource);
+
+            var item = NSObject.FromObject(uiImage);
+            var activityItems = new[] { item };
+            var activityController = new UIActivityViewController(activityItems, null);
+
+            var topController = UIApplication.SharedApplication.KeyWindow.RootViewController;
+
+            while (topController.PresentedViewController != null)
+            {
+                topController = topController.PresentedViewController;
+            }
+
+            topController.PresentViewController(activityController, true, () => { });
         }
     }
 }
